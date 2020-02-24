e51e4e6e81fa5e870a58465320b5d5045d58df63
Robert Cox <robertrcoxjr@yahoo.com>
2020-02-23 23:19:53 -0500

    finished with main assignment

--- sqlalchemy app.py
+++ sqlalchemy app.py
@@ -1,4 +1,4 @@
-# 1. import Flask and all the other dependencies
+#import Flask and all the other dependencies
 from flask import Flask, jsonify
 
 import numpy as np
@@ -10,7 +10,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-# 2. Create an app, being sure to pass __name__
+#Create an app
 app = Flask(__name__)
 
 #Create an engine etc
@@ -30,22 +30,7 @@ firstdate = lastdate
 delta = relativedelta(months = 12)
 firstdate = firstdate - delta
 
-#Calc_temps function to call alter
-# def calc_temps(start_date, end_date):  #can't use this because it will try to call a session outside of where it was created,
-#which results in a crash when using flask like this 
-#     return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
-
-# * `/`
-
-  # * Home page.
-
-  # * List all routes that are available.
-
-
-
-
-
-
+#Create homepage that lists all available routes and how to use them
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
@@ -55,24 +40,12 @@ def home():
         "/api/v1.0/precipitation <br>"
         "/api/v1.0/stations <br> "
         "/api/v1.0/tobs <br> "
-        "/api/v1.0/(start) <br> " #replace <start> and <end> with specific dates!
+        "/api/v1.0/(start) <br> "
         "Usage - replace start with a date in yyyy-mm-dd format <br>"
         "/api/v1.0/(start)/(end) <br>"
         "Usage - replace start and end with a date in yyyy-mm-dd format <br>"
-        "(start) and (start)/(end) return min temperature as 0, avg as 1, max as 2"
     )
-# "Available webpages: "
-        # "/api/v1.0/precipitation, "
-        # "/api/v1.0/stations, "
-        # "/api/v1.0/tobs, "
-        # "/api/v1.0/<start>, "
-        # "/api/v1.0/<start>/<end>, "
-
-        # * `/api/v1.0/precipitation`
-
-  # * Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
-
-  # * Return the JSON representation of your dictionary.
+#Convert the precipitation query's results to a dictionary, then return the JSON representation of the dictionary.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     print("Server received request for 'Precipitation' page...")
@@ -83,61 +56,40 @@ def precipitation():
     #how am I supposed to make a decent dictionary with this when we have multiple values for the same dates,
     #resulting in overwriting the values for each key?
     #I'm really not sure if this is the query the directions actually wanted, but it seems like the only precipitation related one we did
-    
-    #return dt.datetime.strftime(lastdate,'%Y-%m-%d') + "<br>" + str(firstdate)
-    return dict(precipQuery)
-   #precipQuery = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date.desc()).filter(Measurement.date > firstdate).all()
-    
-# * `/api/v1.0/stations`
+    return jsonify(dict(precipQuery))  
 
-  # * Return a JSON list of stations from the dataset.
-
+#return a JSON list of all the stations in the dataset
 @app.route("/api/v1.0/stations")
 def stations():
     print("Server received request for 'Stations' page...")
     session = Session(engine)
-    #session.query(func.count(Station.id)).all()[0][0]
     
     #get the names of each station and return them as json
     stations = session.query(Station.name).all()
     return jsonify(stations)
 
-# * `/api/v1.0/tobs`
-  # * query for the dates and temperature observations from a year from the last data point.
-  # * Return a JSON list of Temperature Observations (tobs) for the previous year.
+#query temperature observations for the past year then return them in a JSON list
 @app.route("/api/v1.0/tobs") 
 def temperature():
     print("Server received request for 'Temperature' page...")
     session = Session(engine)
-    
-    #tempteratures = session.query().filter(Measurement.date > firstdate).all()
-    
-    #Return a JSON list of temperuatre observations for the previous year
-    #another query I'm not particularly sure of, since again we'll have multiple results per date, but a specific station was never specified either
+
+    #another query I'm not particularly sure of, since again we'll have multiple results per date (less of a problem since not a dictionary this time), but a specific station was never specified either
     tempQuery = session.query(Measurement.date, Measurement.tobs).order_by(Measurement.date.asc()).filter(Measurement.date > firstdate).all()
     return jsonify(tempQuery)
 
-# * `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
-
-  # * Return a JSON list of the minimum temperature, the average temperature,
-  #and the max temperature for a given start or start-end range.
-
-  # * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all 
-  #dates greater than and equal to the start date.
-
-  # * When given the start and the end date, calculate the `TMIN`, `TAVG`, and 
-  #`TMAX` for dates between the start and end date inclusive.
-@app.route("/api/v1.0/<start>") #figure out the actual route to put here
+#return a JSON list of min, avg, and max temperature between a given start date and the last observed date
+@app.route("/api/v1.0/<start>") 
 def start(start):
     print("Server received request for 'Start Date' page...")
     session = Session(engine)
-    #values = calc_temps(start, lastdate) - doesn't seem like it likes access of the session coming from a function accessed outside of this function
     #change the labels of 0, 1, and 2 to min, avg, and max. Can simply return jsonify(values) if these labels are unwanted
     keys = ("min", "avg", "max")
     values = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= lastdate).all()[0]
     result = {k:v for k,v in zip(keys,values)}
     return jsonify(result)
-    
+
+#return a JSON list of min, avg, and max temperature between a given start and end date
 @app.route("/api/v1.0/<start>/<end>")
 def startend(start,end):
     print("Server received request for 'Start/End Date' page...")
